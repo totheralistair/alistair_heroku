230c466db6e7d3f5d2c57b8050317f736e4364cd
Allen Maxwell <aamax@xmission.com>
2014-03-31 17:01:29 -0600

    heroku initial commit

---
+++ main.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'rack'
+
+class HelloWorld
+  def call(env)
+    [200, {"Content-Type" => "text/html"}, ["Hello Rack!"]]
+  end
+end
+
+Rack::Handler::WEBrick.run HelloWorld.new, :Port => 9292
